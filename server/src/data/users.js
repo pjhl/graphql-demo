595d95b97b8ac48005d3ee0a67f5885876609aac
pjhl <pjhl.coder@gmail.com>
2018-09-28 22:04:56 +0300

    Added the mock user's profile images

--- server/src/data/users.js
+++ server/src/data/users.js
@@ -2,61 +2,91 @@ const documents = [{
   id: '1',
   name: 'Moshe Laxe',
   username: 'mkellert0',
-  avatar: null
+  avatar: {
+    small: '/images/avatars/64x64.png/5fa2dd/ffffff',
+    medium: '/images/avatars/128x128.jpg/5fa2dd/ffffff'
+  }
 },
 {
   id: '2',
   name: 'Harley Storks',
   'username': 'hpontin1',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.jpg/cc0000/ffffff',
+    'medium': '/images/avatars/128x128.png/dddddd/000000'
+  }
 },
 {
   'id': '3',
   'name': 'Annadiana Lambregts',
   'username': 'asiemens2 Fullick',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.png/dddddd/000000',
+    'medium': '/images/avatars/128x128.bmp/cc0000/ffffff'
+  }
 },
 {
   'id': '4',
   'name': 'Augustine Gorham',
   'username': 'aboller3',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.bmp/5fa2dd/ffffff',
+    'medium': '/images/avatars/128x128.bmp/ff4444/ffffff'
+  }
 },
 {
   'id': '5',
   'name': 'Kerby Whipp',
   'username': 'klilburne4',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.bmp/dddddd/000000',
+    'medium': '/images/avatars/128x128.jpg/dddddd/000000'
+  }
 },
 {
   'id': '6',
   'name': 'Moritz Besnard',
   'username': 'mmacdearmont5',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.png/5fa2dd/ffffff',
+    'medium': '/images/avatars/128x128.png/cc0000/ffffff'
+  }
 },
 {
   'id': '7',
   'name': 'Rad De Bruyne',
   'username': 'rhanes6',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.bmp/ff4444/ffffff',
+    'medium': '/images/avatars/128x128.png/5fa2dd/ffffff'
+  }
 },
 {
   'id': '8',
   'name': 'Edeline Lumsdon',
   'username': 'eshadfourth7',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.jpg/dddddd/000000',
+    'medium': '/images/avatars/128x128.png/cc0000/ffffff'
+  }
 },
 {
   'id': '9',
   'name': 'Weider e-Cartmale',
   'username': 'wsinderson8',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.jpg/ff4444/ffffff',
+    'medium': '/images/avatars/128x128.jpg/cc0000/ffffff'
+  }
 },
 {
   'id': '10',
   'name': 'Dierdre Fullick',
   'username': 'doldis9',
-  'avatar': null
+  'avatar': {
+    'small': '/images/avatars/64x64.jpg/dddddd/000000',
+    'medium': '/images/avatars/128x128.png/dddddd/000000'
+  }
 }]
 
 module.exports = {
