9f304b3de8e56016bb9b65f3ef5e04949d6ab636
pjhl <pjhl.coder@gmail.com>
2018-09-29 00:06:30 +0300

    Added the likes mock data and resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,5 +1,6 @@
 const users = require('./data/users')
 const posts = require('./data/posts')
+const likes = require('./data/likes')
 
 const mockCurrentUserId = '1'
 
@@ -11,6 +12,22 @@ const resolvers = {
     async posts (user) {
       const allPosts = await posts.all()
       return allPosts.filter(el => el.authorId === user.id)
+    }
+  },
+  Post: {
+    async author (post) {
+      return users.getById(post.authorId)
+    },
+    async likes (post) {
+      return likes.getByPostId(post.id)
+    }
+  },
+  Like: {
+    async post (like) {
+      return posts.getById(like.postId)
+    },
+    async user (like) {
+      return users.getById(like.userId)
     }
   }
 }
