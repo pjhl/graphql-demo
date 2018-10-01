93b2a47f5027166bf827d8b351aea77b234e3903
pjhl <pjhl.coder@gmail.com>
2018-10-02 01:43:44 +0300

    Started work on profile page layout

---
+++ frontend/src/components/CreatePost.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (<div>
+  Here will be a create post form
+</div>)
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,23 +2,75 @@ import React, { Component } from 'react'
 import { getUserPage } from '../actions/users'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import { compose } from 'redux'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Divider from '@material-ui/core/Divider'
+import CreatePost from './CreatePost'
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  },
+  paperInner: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: -12
+  },
+  head: {},
+  avatarGrid: {
+    width: 128 + 3 * theme.spacing.unit + 20,
+    maxWidth: 128 + 3 * theme.spacing.unit + 20
+  },
+  name: {
+    marginTop: 0,
+    marginBottom: 6
+  },
+  username: {
+    color: '#424242'
+  },
+  divider: {
+    marginTop: 12,
+    marginBottom: 12
+  }
+})
 
 class Profile extends Component {
   componentDidMount () {
     this.props.getUserPage(1) // TODO: Load current user page
   }
   render () {
-    const { loading, data } = this.props
+    const { loading, data, classes } = this.props
     if (loading) {
       return <div>Loading...</div>
     }
-    return <div>{JSON.stringify(data, undefined, 2)}</div>
+    return <Paper className={classes.root} elevation={1}>
+      <div className={classes.paperInner}>
+        <div className={classes.head}>
+          <Grid container spacing={24}>
+            <Grid item xs={3} className={classes.avatarGrid}>
+              <img src={data.avatar.medium} width={128} height={128} alt='Avatar' />
+            </Grid>
+            <Grid item xs={3}>
+              <h1 className={classes.name}>{data.name}</h1>
+              <div className={classes.username}>@{data.username}</div>
+            </Grid>
+          </Grid>
+          <Divider className={classes.divider} />
+          <CreatePost />
+        </div>
+      </div>
+    </Paper>
   }
 }
 
 Profile.propTypes = {
   loading: PropTypes.bool.isRequired,
-  data: PropTypes.object
+  data: PropTypes.object,
+  classes: PropTypes.object.isRequired
 }
 
 function mapToStateprops (state) {
@@ -28,6 +80,9 @@ function mapToStateprops (state) {
   }
 }
 
-export default connect(mapToStateprops, {
-  getUserPage
-})(Profile)
+export default compose(
+  withStyles(styles),
+  connect(mapToStateprops, {
+    getUserPage
+  })
+)(Profile)
