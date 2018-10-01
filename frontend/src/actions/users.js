71764652c796ba298794190866ac1d9000c664d1
pjhl <pjhl.coder@gmail.com>
2018-10-02 00:46:08 +0300

    Added user fetching from backend

--- frontend/src/actions/users.js
+++ frontend/src/actions/users.js
@@ -1,10 +1,47 @@
-const mockUser = { id: '1' }
+/* global fetch */
 
-export const getUserPage = (id) => dispatch => {
-  setTimeout(() => {
-    dispatch({
-      type: 'USER_PAGE_FETCH_SUCCESS',
-      payload: mockUser
+const grapqlEntrypoint = 'http://localhost:8081/graphql'
+const userFragment = `
+id
+name
+username
+avatar {
+  medium
+}
+posts {
+  author {
+    name
+    avatar {
+      small
+    }
+  }
+  body
+  date
+  likes {
+    total
+    currentUserLiked
+  }
+}`
+
+export const getUserPage = (id) => async dispatch => {
+  const data = await fetch(grapqlEntrypoint, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `{
+        user (id: "${id}") {
+          ${userFragment}
+        }
+      }`
     })
-  }, 1000)
+  })
+    .then(rawResponse => (rawResponse.json()))
+    .then(response => response.data.user)
+  dispatch({
+    type: 'USER_PAGE_FETCH_SUCCESS',
+    payload: data
+  })
 }
